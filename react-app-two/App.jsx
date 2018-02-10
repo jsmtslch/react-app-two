e5ba9b720962daff2d7d040e5f5cc558589546bf
Jasmeet Singh <jasmeet.singh@staples.com>
2018-02-10 10:44:32 -0500

    Events and Forms example

--- react-app-two/App.jsx
+++ react-app-two/App.jsx
@@ -1,33 +1,35 @@
 import React from 'react';
 
-class App extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={
-            data:'Initial Data...'
-        }
-        this.onchangeevent = this.onchangeevent.bind(this);
-    }
-    render(){
-        return(
-            <div>
-                <div>Hello World, again.</div>
-                <InputField fielddata={this.state.data} onchangeevent={this.state.onchangeevent}/>
-            </div>
-        )
-    }
-
-    onchangeevent(){
-        this.state.data = 'a'+this.state.data+'z';
-    }
+class App extends React.Component {
+   constructor(props) {
+      super(props);
+      
+      this.state = {
+         data: 'Initial data...'
+      }
+      this.updateState = this.updateState.bind(this);
+   };
+   updateState(e) {
+      this.setState({data: e.target.value});
+   }
+   render() {
+      return (
+         <div>
+            <Content myDataProp = {this.state.data} 
+               updateStateProp = {this.updateState}></Content>
+         </div>
+      );
+   }
 }
-
-class InputField extends React.Component{
-    render(){
-        return(
-            <div><input type="text" value={this.props.fielddata} onChange={this.props.onchangeevent}></input></div>
-        )
-    }
+class Content extends React.Component {
+   render() {
+      return (
+         <div>
+            <input type = "text" value = {this.props.myDataProp} 
+               onChange = {this.props.updateStateProp} />
+            <h3>{this.props.myDataProp}</h3>
+         </div>
+      );
+   }
 }
-
 export default App;
