c26bd93e456f02f2c59993dd058882b2b6f1a3ba
Jasmeet Singh <jasmeet.singh@staples.com>
2018-02-10 11:04:28 -0500

    Refs examples

--- react-app-two/App.jsx
+++ react-app-two/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
 
 class App extends React.Component {
    constructor(props) {
@@ -8,15 +9,23 @@ class App extends React.Component {
          data: 'Initial data...'
       }
       this.updateState = this.updateState.bind(this);
+      this.clearInput = this.clearInput.bind(this);
    };
    updateState(e) {
       this.setState({data: e.target.value});
+   }
+   clearInput(){
+       this.setState({data:''});
+        //ReactDOM.findDOMNode(this.refs.myInput).focus();
+        ReactDOM.findDOMNode(this.refs.myParentInputRef).focus();
    }
    render() {
       return (
          <div>
             <Content myDataProp = {this.state.data} 
                updateStateProp = {this.updateState}></Content>
+            <button onClick={this.clearInput}>Clear</button>
+            <input type="text" defaultValue={this.state.data} ref="myParentInputRef"/>
          </div>
       );
    }
@@ -26,7 +35,7 @@ class Content extends React.Component {
       return (
          <div>
             <input type = "text" value = {this.props.myDataProp} 
-               onChange = {this.props.updateStateProp} />
+               onChange = {this.props.updateStateProp} ref="myInput"/>
             <h3>{this.props.myDataProp}</h3>
          </div>
       );
