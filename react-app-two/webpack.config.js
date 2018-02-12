8a12e786f98571aeece2910b71b3f8c08148cbae
Jasmeet Singh <jasmeet.singh@staples.com>
2018-02-12 12:47:25 -0500

    Router Example

--- react-app-two/webpack.config.js
+++ react-app-two/webpack.config.js
@@ -1,24 +1,18 @@
-var config = {
-    entry: './main.js',
+module.exports = {
+    entry: './App/main.js',
     output: {
-       path:'/',
-       filename: 'index.js',
-    },
-    devServer: {
-       inline: true,
-       port: 8080
+       filename: 'bundle.js'
     },
     module: {
        loaders: [
           {
-             test: /\.jsx?$/,
-             exclude: /node_modules/,
              loader: 'babel-loader',
-             query: {
-                presets: ['es2015', 'react']
-             }
+             test: /\.js$/,
+             exclude: /node_modules/
           }
        ]
+    },
+    devServer: {
+       port: 7777
     }
- }
- module.exports = config;
+ };
